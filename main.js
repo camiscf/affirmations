618779264cb627240b10dbd0c544c9458bd68915
Camila <87675978+camiscf@users.noreply.github.com>
2022-08-10 14:55:49 -0300

    :heart: updating main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById("fetch").addEventListener("click", fetchPhrase);
 
 function fetchPhrase(){
-    fetch('/affirmation_api/affirmations.txt')
+    fetch('affirmations.txt')
         .then(response => response.text())
         .then(data => {
             const line = data.split('\n')[Math.floor(Math.random() * data.split('\n').length)];
